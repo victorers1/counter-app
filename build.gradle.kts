88c06e4d14424cbaa947b9d82fcc4b51ccd5ca39
Victor Emanuel <victor.emanuel@iteris.com.br>
2024-10-10 14:11:11 -0300

    chore: updates IDE files

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.compose.compiler) apply false
-    id("com.google.devtools.ksp") version "2.0.20-1.0.24" apply false
+    id("com.google.devtools.ksp") version "2.0.20-1.0.25" apply false
     id("com.google.dagger.hilt.android") version "2.52" apply false
     id("com.google.protobuf") version "0.9.4" apply false
 }
